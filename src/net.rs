229ded45814db4596e1799202aafa1dc0974e54b
Bart Massey <bart@cs.pdx.edu>
2019-08-03 14:00:05 -0700

    added telnet ANSI terminal negotiation

--- src/net.rs
+++ src/net.rs
@@ -6,6 +6,27 @@ use TelnetOption::*;
 
 use std::io::*;
 use std::net::*;
+use std::collections::HashSet;
+
+// Terminal type information from
+// https://code.google.com/archive/p/bogboa/wikis/TerminalTypes.wiki
+const TTYPES: &[&'static str] = &[
+    "ansi",
+    "xterm",
+    "eterm",
+    "rxvt",
+    "tintin++",
+    "gosclient",
+    "mushclient",
+    "zmud",
+    "gosclient",
+    "vt1",
+    "tinyfugue",
+];
+
+// TTYPE subnegotiation commands.
+const SEND: u8 = 1;
+const IS: u8 = 0;
 
 #[derive(Debug)]
 pub struct NegotiationError;
@@ -22,20 +43,30 @@ impl std::error::Error for NegotiationError {
     }
 }
 
-pub struct Connection(Telnet);
+pub struct Connection {
+    telnet: Telnet,
+    ttypes: HashSet<String>,
+    pub ansi: bool,
+}
 
 impl Connection {
     pub fn new(stream: TcpStream) -> Connection {
         let mut telnet = Telnet::from_stream(Box::new(stream), 256);
         telnet.negotiate(Will, SuppressGoAhead);
         telnet.negotiate(Will, Echo);
-        Connection(telnet)
+        telnet.negotiate(Do, TTYPE);
+        Connection { telnet, ttypes: HashSet::new(), ansi: false }
+    }
+
+    fn negotiate_ansi(&mut self) {
+        self.telnet.subnegotiate(TelnetOption::TTYPE, &[SEND]);
     }
 
     pub fn read(&mut self) -> io::Result<String> {
-        loop {
+        'eventloop: loop {
+            let event = self.telnet.read()?;
             use TelnetEvent::*;
-            match self.0.read()? {
+            match event {
                 Data(buf) => match String::from_utf8(buf.to_vec()) {
                     Ok(s) => return Ok(s),
                     Err(e) => {
@@ -46,19 +77,37 @@ impl Connection {
                     }
                 },
                 TimedOut => panic!("unexpected telnet read timeout"),
-                NoData => panic!("unexpected telnet read nodata"),
+                NoData => eprintln!("unexpected telnet read nodata"),
                 Error(msg) => {
                     panic!("unexpected telnet read error: {}", msg)
                 }
                 Negotiation(Do, SuppressGoAhead) => (),
                 Negotiation(Do, Echo) => (),
-                neg => {
-                    eprintln!("{:?}", neg);
-                    return Err(io::Error::new(
-                        ErrorKind::InvalidData,
-                        NegotiationError,
-                    ));
-                }
+                Negotiation(Will, TTYPE) => self.negotiate_ansi(),
+                Negotiation(Wont, TTYPE) => eprintln!("terminal wont ANSI"),
+                Subnegotiation(TTYPE, buf) => {
+                    assert_eq!(buf[0], IS);
+                    let ttype = std::str::from_utf8(&buf[1..]).unwrap().to_string();
+                    for good_ttype in TTYPES {
+                        let ttype = ttype.to_lowercase();
+                        if ttype.len() < good_ttype.len() {
+                            continue;
+                        }
+                        if &ttype[0..good_ttype.len()] == *good_ttype {
+                            eprintln!("got ANSI terminal");
+                            self.ansi = true;
+                            continue 'eventloop;
+                        }
+                    }
+                    if self.ttypes.contains(&ttype) {
+                        eprintln!("terminal cannot ANSI");
+                        continue 'eventloop;
+                    }
+                    eprintln!("unloved terminal: {}", ttype);
+                    self.ttypes.insert(ttype);
+                    self.telnet.subnegotiate(TTYPE, &[SEND]);
+                },
+                neg => eprintln!("unexpected negotation: {:?}", neg),
             }
         }
     }
@@ -66,7 +115,7 @@ impl Connection {
 
 impl Write for Connection {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.0.write(buf)
+        self.telnet.write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
